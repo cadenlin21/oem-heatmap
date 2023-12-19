81562fd6fbd1bf6d81f320cb736e2fee7d53e3e7
ayangnath <88808124+ayangnath@users.noreply.github.com>
2023-12-18 20:47:54 -0800

    coords

--- main.py
+++ main.py
@@ -44,10 +44,39 @@ company_locations = {
         'name': ['Medford, OR', 'San Diego, CA', 'San Gabriel, CA', 'Santa Clarita, CA', 'Placerville, CA', 'Hayward, CA', 'Monterey, CA']
     }),
     'McCain': pd.DataFrame({
-        'lat': [33.4242, 30.0933, 34.0536, 30.5066, 33.1580, 32.9545, 42.6511, 42.8867, 34.1397, 41.1670, 37.7790, 39.7392, 32.7531, 32.8140, 38.9012, 33.4709, 33.4151, 33.1433, 30.2805, 44.9374, 35.4495, 33.7554, 41.5638, 34.3207, 29.8572, 42.3602, 36.0652, 37.3347, 36.3955, 36.1881, 47.1853, 34.9323],
-        'lon': [-111.9280, -95.9886, -118.2427, -97.8303, -117.3505, -97.0150, -73.7549, -78.8783, -118.0353, -73.2048, -122.4199, -104.9848, -97.3327, -96.9488, -77.2652, -81.9748, -111.8314, -117.1661, -97.7389, -123.0272, -97.3967, -84.3883, -93.7593, -95.3930, -71.0582, -119.0167, -112.0741, -122.0090, 97.8783, -94.5404, -122.2928, -95.7697],
-        'name': ['Arizona State University, AZ', 'Prairie View A&M University, TX', 'Los Angeles, CA', 'Cedar Park, TX', 'Carlsbad, CA', 'Coppell, TX', 'Albany, NY', 'Buffalo, NY', 'Arcadia, CA', 'Bridgeport, CT', 'San Francisco, CA', 'Denver, CO', 'Fort Worth, TX', 'Irving, TX', 'Vienna, VA', 'Augusta, GA', 'Mesa, AZ', 'San Marcos, CA', 'Texas DOT, TX', 'Oregon DOT, OR', 'Midwest City, OK', 'Georgia DOT, GA', 'West Des Moines, IA', 'Harris County, TX', 'Massachusetts DOT, MA', 'Porterville, CA', 'Phoenix, AZ', 'Apple Campus, Cupertino, CA', 'Enid, OK', 'Siloam Springs, AR', 'Puyallup, WA', 'McAlester, OK']
-    }),
+        'lat': [
+        33.4242, 30.0933, 34.0536, 30.5066, 33.1580, 32.9545, 42.6511, 42.8867, 
+        34.1397, 41.1670, 37.7790, 39.7392, 32.7531, 32.8140, 38.9012, 33.4709, 
+        33.4151, 33.1433, 30.2805, 44.9374, 35.4495, 33.7554, 41.5638, 34.3207, 
+        29.8572, 42.3602, 36.0652, 37.3347, 36.3955, 36.1881, 47.1853, 34.9323,
+        35.8681455, 30.2711286, 32.715738, 33.1031744, 39.1156615, 42.7284117,
+        43.0046095, 43.2128473, 43.0386671, 43.0481221, 35.247287, 35.2578503,
+        36.1867442, 36.3320196, 36.0625795, 41.4302973, 40.336927
+        ],
+        'lon': [
+        -111.9280, -95.9886, -118.2427, -97.8303, -117.3505, -97.0150, -73.7549, 
+        -78.8783, -118.0353, -73.2048, -122.4199, -104.9848, -97.3327, -96.9488, 
+        -77.2652, -81.9748, -111.8314, -117.1661, -97.7389, -123.0272, -97.3967, 
+        -84.3883, -93.7593, -95.3930, -71.0582, -119.0167, -112.0741, -122.0090, 
+        97.8783, -94.5404, -122.2928, -95.7697, -83.561835, -97.7436995, -117.1610838, 
+        -96.6705503, -77.5636015, -73.6917851, -76.1997126, -75.4557304, -78.8642035, 
+        -76.1474244, -97.5997601, -96.9366989, -94.1288142, -94.1185366, -94.1574263, 
+        -97.3593904, -104.9121967
+        ],
+        'name': [
+        'Arizona State University, AZ', 'Prairie View A&M University, TX', 'Los Angeles, CA', 
+        'Cedar Park, TX', 'Carlsbad, CA', 'Coppell, TX', 'Albany, NY', 'Buffalo, NY', 
+        'Arcadia, CA', 'Bridgeport, CT', 'San Francisco, CA', 'Denver, CO', 'Fort Worth, TX', 
+        'Irving, TX', 'Vienna, VA', 'Augusta, GA', 'Mesa, AZ', 'San Marcos, CA', 'Texas DOT, TX', 
+        'Oregon DOT, OR', 'Midwest City, OK', 'Georgia DOT, GA', 'West Des Moines, IA', 
+        'Harris County, TX', 'Massachusetts DOT, MA', 'Porterville, CA', 'Phoenix, AZ', 
+        'Apple Campus, Cupertino, CA', 'Enid, OK', 'Siloam Springs, AR', 'Puyallup, WA', 
+        'McAlester, OK', 'Sevierville, TN', 'Austin, TX', 'San Diego County, CA', 'Allen, TX', 
+        'Leesburg, VA', 'Troy, NY', 'Onondaga County, NY', 'Rome, NY', 'North Tonawanda, NY', 
+        'Syracuse, NY', 'Newcastle, OK', 'Tecumseh, OK', 'Springdale, AR', 'Rogers, AR', 
+        'Fayetteville, AR', 'Columbus, NE', 'Johnstown, CO'
+        ]
+        }),
 
 }
 # company_locations = {
